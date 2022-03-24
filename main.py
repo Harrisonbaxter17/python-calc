730ece4d0d1a7986d3875989d73754c13b5183c7
Harrisonbaxter17 <94960385+Harrisonbaxter17@users.noreply.github.com>
2022-03-24 23:22:01 +0000

    Update main.py
    added a function for the sqaure root feature

--- main.py
+++ main.py
@@ -1,25 +1,53 @@
-import time
+import time 
 import math
 import sys
+
 def calc():
-    print("------------------------------------------------------")
-    print("-----------WELCOME TO THE PYTHON CALCULATOR-----------")
-    print("------------------------------------------------------")
 
-    operator = input("""please chose an operator:
-        + for addition
-        - for subtraction
-        * for multiplication
-        / for division
-        <> for square root of (x)
-        : """)
-        
-    number1 = input("enter the first number: ")
+    choice = input("""please chose which number you would like to perform:
 
-    print()
+-------------------------
+--------Number 1---------
+-------------------------
 
-    number2 = input("enter the second number: ")
++ for addition 
+- for subtraction
+* for multiplication
+/ for division
+_________________________
+
+
+-------------------------
+--------Number 2---------
+-------------------------
+
+<> for square root of (x)
+_________________________
+
+: """)
+
+
+    if choice == "1":
+        Basic_Math()
+    elif choice == "2":
+        Advanced_math()
+    else:
+        print("you typed an incorrect operator, please wait to restart!")
+        time.sleep(3)
+        calc()
     
+def Basic_Math():
+
+    operation = input("""please chose an operator:
+    + for addition
+    - for subtraction
+    * for multiplication
+    / for division
+    : """)
+
+    number1 = input("please enter the first number: ")
+    print()
+    number2 = input("please enter the second number: ")
 
     def addition():
         total = float(number1) + float(number2)
@@ -37,31 +65,29 @@ def calc():
         total = float(number1) / float(number2)
         print(f"{number1} / {number2} = {total}")
 
-
-    if operator == "+":
+    if operation == "+":
         addition()
-    elif operator == "-":
+    elif operation == "-":
         subtraction()
-    elif operator == "*":
+    elif operation == "*":
         multiplication()
-    elif operator == "/":
+    elif operation == "/":
         division()
     else:
         print("you have typed an incorrect operator")
 
     again()
 
-
 def again():
     calculator_again = input("""
-    do you want to use the calculator again?
-    please type Y for YES or N for NO
-    """).upper()
+do you want to use the calculator again?
+type Y for YES or N for NO
+""").upper()
 
     if calculator_again == "Y":
         calc()
     elif calculator_again == "N":
-        print("THANK-YOU FOR USING THE PYTHON CALCULATOR, GOODBYE")
+        print("THANK-YOU FOR USING THE PYTHON CALCULATOR, GOODBYE!")
         print("EXITING.")
         time.sleep(0.5)
         print("EXITING..")
@@ -71,10 +97,54 @@ def again():
         print("EXITING....")
         time.sleep(0.5)
         print("EXITING.....")
+        time.sleep(0.5)
+        print("EXITING....")
+        time.sleep(0.5)
+        print("EXITING...")
         sys.exit(0)
+
+
+
+def Advanced_math():
+
+    operation = input("""please chose an operator:
+    <> for the square root of (n)
+    : """)
         
+    number1 = input("please enter a number to perfrom the square root on: ")
+
+    def sqrt():
+        total = math.sqrt(float(number1))
+        print(str(total))
+        
+    if operation == "<>":
+        sqrt()
+    again()
+
+def again():
+    calculator_again = input("""
+do you want to use the calculator again?
+type Y for YES or N for NO
+""").upper()
+
+    if calculator_again == "Y":
+        calc()
+    elif calculator_again == "N":
+        print("THANK-YOU FOR USING THE PYTHON CALCULATOR, GOODBYE!")
+        print("EXITING.")
+        time.sleep(0.5)
+        print("EXITING..")
+        time.sleep(0.5)
+        print("EXITING...")
+        time.sleep(0.5)
+        print("EXITING....")
+        time.sleep(0.5)
+        print("EXITING.....")
+        time.sleep(0.5)
+        print("EXITING....")
+        time.sleep(0.5)
+        print("EXITING...")
+        sys.exit(0)
+
 
 calc()
-
-
-
