6e7e994e6b8b2e04ebe33b1aa7393697c7cefc4d
Harrisonbaxter17 <94960385+Harrisonbaxter17@users.noreply.github.com>
2022-04-04 21:16:04 +0100

    Update main.py
    completely rehauled the layout of the calculator, new features are:
    .new layout
    .more mathematical option to choose from

--- main.py
+++ main.py
@@ -1,150 +1,168 @@
-import time 
-import math
 import sys
+import time
+import math
+
 
 def calc():
+    def welcome():
+        print()
+        print("--------------------------------")
+        print("WELCOME TO THE PYTHON CALCULATOR")
+        print("--------------------------------")
+        print()
 
-    choice = input("""please chose which number you would like to perform:
+    welcome()
 
--------------------------
---------Number 1---------
--------------------------
-
-+ for addition 
-- for subtraction
-* for multiplication
-/ for division
-_________________________
-
-
--------------------------
---------Number 2---------
--------------------------
-
-<> for square root of (x)
-_________________________
-
--------------------------
---------Number 3---------
--------------------------
-
-(PI) for displaying PI 
-_________________________
-
-: """)
-
-
-    if choice == "1":
-        Basic_Math()
-    elif choice == "2":
-        Advanced_math()
-    else:
-        print("you typed an incorrect operator, please wait to restart!")
-        time.sleep(3)
-        calc()
-    
-def Basic_Math():
-
-    print("-------------------------")
-    operation = input("""please chose an operator:
-    + for addition
-    - for subtraction
-    * for multiplication
-    / for division
+    choice = input("""please choose a number to perform:
+    1. addition 
+    2. subtraction
+    3. multiplication 
+    4. division
+    5. √ (square root)
+    6. x²
+    7. a²+b² = c² (Pythagorean Theorem)
     : """)
 
-    print("--------------------------------")
-    
-    number1 = input("please enter the first number: ")
-    print("--------------------------------")
-    number2 = input("please enter the second number: ")
-    print("--------------------------------")
-
-    def addition():
+    def add():
+        number1 = input("please enter the first number: ")
+        number2 = input("please enter the first number: ")
         total = float(number1) + float(number2)
         print(f"{number1} + {number2} = {total}")
 
-    def subtraction():
+    def sub():
+        number1 = input("please enter the first number: ")
+        number2 = input("please enter the first number: ")
         total = float(number1) - float(number2)
         print(f"{number1} - {number2} = {total}")
 
-    def multiplication():
+    def mult():
+        number1 = input("please enter the first number: ")
+        number2 = input("please enter the first number: ")
         total = float(number1) * float(number2)
         print(f"{number1} * {number2} = {total}")
 
-    def division():
+    def div():
+        number1 = input("please enter the first number: ")
+        number2 = input("please enter the first number: ")
         total = float(number1) / float(number2)
         print(f"{number1} / {number2} = {total}")
 
-    if operation == "+":
-        addition()
-    elif operation == "-":
-        subtraction()
-    elif operation == "*":
-        multiplication()
-    elif operation == "/":
-        division()
-    else:
-        print("you have typed an incorrect operator")
-
-    again()
-
-def again():
-    calculator_again = input("""
-do you want to use the calculator again?
-type Y for YES or N for NO
-""").upper()
-
-    if calculator_again == "Y":
-        calc()
-    elif calculator_again == "N":
-        print("THANK-YOU FOR USING THE PYTHON CALCULATOR, GOODBYE!")
-        print("EXITING.")
-        time.sleep(0.5)
-        print("EXITING..")
-        time.sleep(0.5)
-        print("EXITING...")
-        time.sleep(0.5)
-        print("EXITING....")
-        time.sleep(0.5)
-        print("EXITING.....")
-        time.sleep(0.5)
-        print("EXITING....")
-        time.sleep(0.5)
-        print("EXITING...")
-        sys.exit(0)
-
-
-
-def Advanced_math():
-
-    print("-------------------------")
-    
-    operation = input("""please chose an operator:
-    <> for the square root of (n)
-    : """)
-        
-    number1 = input("please enter a number: ")
-
-    def sqrt():
+    def root():
+        number1 = input("please enter a number to square root: ")
         total = math.sqrt(float(number1))
-        print(f"the √ of {number1} =  {total}" )
-        
-    if operation == "<>":
-        sqrt()
+        print(f"the √ of {number1} =  {total}")
+
+    def square():
+        x = float(input("please enter a number to square: "))
+        y = float(input("please enter the power number (x²): "))
+        total = math.pow(x, y)
+        print(f"{x} ** {y} = {total}")
+
+    def a_Pythag():
+        c = float(input("enter the length of C: "))
+        b = float(input("enter the length of B: "))
+        a_squared = c ** 2 - b ** 2
+        a = math.sqrt(a_squared)
+        print(f"{c}² - {b}² = {a_squared}")
+        print(f"√ {a_squared} = {a}")
+        print(f"a = {a}")
+
+    def b_Pythag():
+        c = float(input("enter the length of C: "))
+        a = float(input("enter the length of A: "))
+        b_squared = c ** 2 - a ** 2
+        b = math.sqrt(b_squared)
+        print(f"{c}² - {a}² = {b_squared}")
+        print(f"√ {b_squared} = {b}")
+        print(f"b = {b}")
+
+    def c_Pythag():
+        a = float(input("enter the length of A: "))
+        b = float(input("enter the length of B: "))
+        c_squared = a ** 2 + b ** 2
+        c = math.sqrt(c_squared)
+        print(f"{a}² + {b}² = {c_squared}")
+        print(f"√ {c_squared} = {c}")
+        print(f"c = {c}")
+
+    def pythagorean():
+        print("""
+                |\          
+                | \       
+                |  \      
+                |   \     
+           A    |    \    C
+                |     \   
+                |      \  
+                |       \ 
+                ---------        
+                
+                    B
+""")
+        selection = input("""please type the letter you want to calculate the value of?
+        side A
+        side B
+        side C
+        : """).upper()
+
+        if selection == "A":
+            a_Pythag()
+        elif selection == "B":
+            b_Pythag()
+        elif selection == "C":
+            c_Pythag()
+        else:
+            print("you have entered an incorrect option!, please wait")
+            time.sleep(2)
+            pythagorean()
+
+    if choice == "1":
+        add()
+    elif choice == "2":
+        sub()
+    elif choice == "3":
+        mult()
+    elif choice == "4":
+        div()
+    elif choice == "5":
+        root()
+    elif choice == "6":
+        square()
+    elif choice == "7":
+        pythagorean()
+    else:
+        print("you have entered an incorrect option, please wait to restart!")
+        time.sleep(3)
+        print("LOADING.")
+        time.sleep(0.5)
+        print("LOADING..")
+        time.sleep(0.5)
+        print("LOADING...")
+        time.sleep(0.5)
+        print("LOADING....")
+        time.sleep(0.5)
+        calc()
 
     again()
 
+
 def again():
     calculator_again = input("""
-do you want to use the calculator again?
-type Y for YES or N for NO
-""").upper()
+    do you want to use the calculator again?
+    type Y for YES or N for N
+    : """).upper()
 
     if calculator_again == "Y":
+        time.sleep(1.5)
+        print("LOADING.")
+        time.sleep(0.5)
+        print("LOADING..")
+        time.sleep(0.5)
+        print("LOADING...")
+        time.sleep(0.5)
         calc()
     elif calculator_again == "N":
-        print("THANK-YOU FOR USING THE PYTHON CALCULATOR, GOODBYE!")
+        print("THANKYOU FOR USING THE PYTHON CALCULATOR, GOODBYE!")
         print("EXITING.")
         time.sleep(0.5)
         print("EXITING..")
@@ -159,10 +177,6 @@ type Y for YES or N for NO
         time.sleep(0.5)
         print("EXITING...")
         sys.exit(0)
-
-def PI():
-
-    print
 
 
 calc()
